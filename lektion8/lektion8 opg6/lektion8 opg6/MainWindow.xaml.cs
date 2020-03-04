57746a10d88fee65f8fd99e564c74068968dcdda
Johan Pedersen <sandbkpedersen.johan@gmail.com>
2020-03-04 17:44:28 +0100

    WPF exercises

--- lektion8/lektion8 opg6/lektion8 opg6/MainWindow.xaml.cs
+++ lektion8/lektion8 opg6/lektion8 opg6/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,76 @@ namespace lektion8_opg6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<User> users = new List<User>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string user in File.ReadAllLines(openFileDialog.FileName))
+                {
+                    string[] userArr = user.Split(';');
+                    User u = new User(int.Parse(userArr[0]), userArr[1], int.Parse(userArr[2]), int.Parse(userArr[3]));
+                    users.Add(u);
+                    userListBox.Items.Add(u.ToString());
+                }
+            }
+        }
+
+        private void lBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (ListBox)sender;
+
+            int userID = int.Parse(item.SelectedItem.ToString().Split(' ')[0]);
+
+
+            foreach(User u in users)
+            {
+                if(u.Id == userID)
+                {
+                    txBoxName.Text = u.Name;
+                    txBoxAge.Text = u.Age.ToString();
+                    txBoxId.Text = u.Id.ToString();
+                    txBoxScore.Text = u.Score.ToString();
+                }
+            }
+
+          
+
+        }
+    }
+
+
+    public class User
+    {
+
+        public User(int id, string name, int age, int score)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Score = score;
+        }
+
+        public int Id { get; set; }
+        public string Name { get ; set ; }
+        public int Age { get ; set ; }
+        public int Score { get ; set ; }
+
+        //Lav properites af attributter
+
+
+
+        public override string ToString()
+        {
+            return Id + " " + Name;
         }
     }
 }
---
+++ lektion8/lektion8 opg7/lektion8 opg7/lektion8 opg7/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lektion8_opg7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<User> users = new List<User>();
+        private User selectedUser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            /**
+             * huske at sætte data konteksten for grid
+             */
+
+            userListBox.ItemsSource = users;
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string user in File.ReadAllLines(openFileDialog.FileName))
+                {
+                    string[] userArr = user.Split(';');
+                    User u = new User(int.Parse(userArr[0]), userArr[1], int.Parse(userArr[2]), int.Parse(userArr[3]));
+                    users.Add(u);
+                }
+
+                userListBox.Items.Refresh();
+            }
+        }
+
+        /**
+         * returner user-id for den user man har trykket på
+         */
+        private void selectUser(ListBox sender)
+        {
+           int userID = int.Parse(sender.SelectedItem.ToString().Split(' ')[0]);
+
+            foreach (User u in users)
+            {
+                if(userID == u.Id)
+                {
+                    selectedUser = u;
+                    break;
+                }
+            }
+
+            gridName.DataContext = selectedUser;
+        }
+        
+
+        private void lBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            selectUser((ListBox)sender);
+
+                    txBoxName.Text = selectedUser.Name;
+                    txBoxAge.Text = selectedUser.Age.ToString();
+                    txBoxId.Text = selectedUser.Id.ToString();
+                    txBoxScore.Text = selectedUser.Score.ToString();
+
+        }
+
+        private void changeAttribute_Click(object sender, RoutedEventArgs e)
+        {
+            if(selectedUser != null)
+            {
+                selectedUser.Name = txBoxName.Text;
+                selectedUser.Age= int.Parse(txBoxAge.Text);
+                MessageBox.Show(txBoxName.Text);
+                selectedUser.Score = int.Parse(txBoxScore.Text);
+
+                MessageBox.Show("Cannot change ID");
+
+                userListBox.Items.Refresh();
+            }
+
+
+        }
+
+
+    }
+
+
+    public class User : INotifyPropertyChanged
+
+    {
+        private int id;
+        private string name;
+        private int age;
+        private int score;
+
+        public User(int id, string name, int age, int score)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.score = score;
+        }
+
+        public int Id {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                NotifyPropertyChanged("Id");
+            }
+                
+                }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+                NotifyPropertyChanged("Score");
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            return Id + " " + Name;
+        }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
+}
